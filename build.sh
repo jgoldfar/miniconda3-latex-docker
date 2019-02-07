0666c0c31093ff42e32442d8ac21d31125522b2b
Jonathan Goldfarb <jgoldfar@gmail.com>
2019-02-07 15:54:00 -0500

    Fixup dev image build, which will now run automatically on Docker Hub when CI runs. It does take 3 hours...

--- build.sh
+++ build.sh
@@ -25,3 +25,11 @@ IMG_TAG=and-maxima-latest
 
 docker build -f and-maxima/Dockerfile -t ${DOCKER_USERNAME}/${DOCKER_REPO_BASE}:${IMG_TAG} and-maxima
 docker push ${DOCKER_USERNAME}/${DOCKER_REPO_BASE}:${IMG_TAG}
+
+# Use Docker Hub's infrastructure to build dev image
+curl -X POST -L https://cloud.docker.com/api/build/v1/source/54b9fdcd-0dd1-4f42-b52e-555b73a2b897/trigger/5f52cd23-427c-483c-b7a5-e491d9f486ec/call/
+
+#IMG_TAG=dev
+
+#docker build -f dev/Dockerfile -t ${DOCKER_USERNAME}/${DOCKER_REPO_BASE}:${IMG_TAG} dev
+#docker push ${DOCKER_USERNAME}/${DOCKER_REPO_BASE}:${IMG_TAG}
