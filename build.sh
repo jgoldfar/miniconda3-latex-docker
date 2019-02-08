47fef8b3316db8aca0dcfe52916ab07933852419
Jonathan Goldfarb <jgoldfar@gmail.com>
2019-02-08 15:54:25 -0500

    Add "full" build to CI

--- build.sh
+++ build.sh
@@ -3,13 +3,15 @@ set -euo pipefail
 IFS=$'\n\t'
 
 DOCKER_REPO_BASE="miniconda3-latex"
-IMG_TAG=full
-
-#docker build -f Dockerfile.${IMG_TAG} -t ${DOCKER_USERNAME}/${DOCKER_REPO_BASE}:${IMG_TAG} .
-#docker push ${DOCKER_USERNAME}/${DOCKER_REPO_BASE}:${IMG_TAG}
-
 
 IMG_TAG=minimal
 
 docker build -f Dockerfile.${IMG_TAG} -t ${DOCKER_USERNAME}/${DOCKER_REPO_BASE}:${IMG_TAG} .
 docker push ${DOCKER_USERNAME}/${DOCKER_REPO_BASE}:${IMG_TAG}
+
+IMG_TAG=full
+
+docker build -f Dockerfile.${IMG_TAG} -t ${DOCKER_USERNAME}/${DOCKER_REPO_BASE}:${IMG_TAG} .
+docker push ${DOCKER_USERNAME}/${DOCKER_REPO_BASE}:${IMG_TAG}
+
+
